a6224906198a02528e156f1b66dc2204fe25e976
Harrison Brock <harrison@harrisonbrock.com>
2019-02-11 16:09:11 -0500

    added route /names/begin

--- src/main/java/com/harrisonbrock/countryapp/controllers/CountryController.java
+++ src/main/java/com/harrisonbrock/countryapp/controllers/CountryController.java
@@ -3,10 +3,12 @@ package com.harrisonbrock.countryapp.controllers;
 import com.harrisonbrock.countryapp.CountryAppApplication;
 import com.harrisonbrock.countryapp.domain.Country;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CountryController {
@@ -16,5 +18,17 @@ public class CountryController {
 
         CountryAppApplication.countryList.countryList.sort(Comparator.comparing(Country::getName));
         return CountryAppApplication.countryList.countryList;
+    }
+
+    @RequestMapping("/names/begin")
+    public List<Country> getCountriesLike(@RequestParam(value = "letters") String letters) {
+        CountryAppApplication.countryList.countryList.sort(Comparator.comparing(Country::getName));
+
+        List<Country> newList;
+        newList = CountryAppApplication
+                .countryList.countryList
+                .stream()
+                .filter(c -> c.getName().toLowerCase().startsWith(letters.toLowerCase())).collect(Collectors.toList());
+        return newList;
     }
 }
